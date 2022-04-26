8c8d7f176f8177243c230f00fd825b29fc21499a
prikachikovim <prikazchikovim@63pokupki.ru>
2022-04-26 19:54:01 +0400

    init

---
+++ src/review/review.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ReviewModel } from './review.model';
+
+@Controller('review')
+export class ReviewController {
+  @Post('create')
+  async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {}
+
+  @Get('byProduct/:product_id')
+  async get(@Param('product_id') productId: string) {}
+}
