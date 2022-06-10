c08a273f3ca9fed683324b0f69e26c221579f1c2
Ilya Prikazchikov <ilyaprikaz@gmail.com>
2022-06-11 03:00:05 +0400

    feat: create files module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { ReviewModule } from './review/review.module';
     TopPageModule,
     ProductModule,
     ReviewModule,
+    FilesModule,
   ],
 })
 export class AppModule {}
