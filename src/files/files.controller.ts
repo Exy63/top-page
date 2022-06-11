aba9a7502518f80e8e7351b8d1db54e2815396e0
Ilya Prikazchikov <ilyaprikaz@gmail.com>
2022-06-11 12:09:17 +0400

    feat: add webp convertation

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -10,6 +10,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { FileElementResponse } from './dto/file-element.response';
 import { FilesService } from './files.service';
+import { MFile } from './mfile.class';
 
 @Controller('files')
 export class FilesController {
@@ -21,6 +22,18 @@ export class FilesController {
   async uploadFile(
     @UploadedFile() file: Express.Multer.File,
   ): Promise<FileElementResponse[]> {
-    return this._filesService.saveFiles([file]);
+    const saveArray: MFile[] = [new MFile(file)];
+
+    if (file.mimetype.includes('image')) {
+      const buffer = await this._filesService.convertToWebP(file.buffer);
+      saveArray.push(
+        new MFile({
+          originalname: `${file.originalname.split('.')[0]}.webp`,
+          buffer,
+        }),
+      );
+    }
+
+    return this._filesService.saveFiles(saveArray);
   }
 }
